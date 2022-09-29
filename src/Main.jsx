dcfd86e8a4c8e76c4f369d32f8167f79ce65a007
Akhmed <ahmedchahkiev001@gmail.com>
2022-09-29 15:01:28 +0300

    Sharingan

---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import logo from './img.png'
+
+const Main = () => {
+    return <main>
+        <h1>sharingan</h1>
+        <div className="image"><img src={logo} alt="sharingan" /></div>
+    </main>
+}
+
+export default Main
